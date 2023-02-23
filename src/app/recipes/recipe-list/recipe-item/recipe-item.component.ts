c84935ed93a6e24a58f2ade979058ce2432adc1f
diegogglez <diegomiller7@gmail.com>
2023-02-23 18:45:01 +0100

    Feat: passing dynamic parameters to recipe links and styling active recipe selected button

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -11,6 +11,7 @@ import { Recipe } from './../../recipe.model';
 export class RecipeItemComponent {
 
   @Input() recipe: Recipe;
+  @Input() index: number;
 
   constructor(private recipeService: RecipeService) {}
 
